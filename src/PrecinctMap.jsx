5b1354c9f101e72997efc08301307ea360de91dc
Rob Cohen <robcohen@users.noreply.github.com>
2023-09-08 01:54:21 +0000

    fix

--- src/PrecinctMap.jsx
+++ src/PrecinctMap.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { MapContainer, TileLayer, GeoJSON, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 import geoJSON from '../static/geo.json';
 import "leaflet/dist/leaflet.css";
+import Airtable from 'airtable'
 import LeafletControlGeocoder from "./LeafletControlGeocoder";
-var airtableKey   = process.env.AIRTABLE_KEY;
+import env from "react-dotenv";
 const geoJSONData = geoJSON;
+console.log(env.AIRTABLE_KEY);
+console.log(env.AIRTABLE_BASE);
+console.log(env.AIRTABLE_TABLE);
 
 export default function PrecinctMap() {
+    async function GetAirtableRecords() {
+      const base = new Airtable({apiKey: env.AIRTABLE_KEY}).base(env.AIRTABLE_BASE);
+      const records = await base(env.AIRTABLE_TABLE).select().all();
+
+      return records;
+    }
+
+    const records = GetAirtableRecords()
+      .then((res)=>console.log("DONE: ", res))
+      .catch((err)=>{console.log("ERR: ", err)});
+
+
 
     const onEachFeature = (feature, layer) => {
         if (feature.properties && feature.properties.Precinct) {
