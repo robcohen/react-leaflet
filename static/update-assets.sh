7366340014e8af40ac82f08186e178a785c5087b
robcohen <robcohen@users.noreply.github.com>
2023-09-10 16:44:34 -0500

    Finished script

--- static/update-assets.sh
+++ static/update-assets.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-GEOJSON="https://taxmaps.traviscountytx.gov/arcgis/rest/services/Precincts/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryPolyline&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnExceededLimitFeatures=false&quantizationParameters=&returnCentroid=false&sqlFormat=none&resultType=&featureEncoding=esriDefault&datumTransformation=&f=geojson"
-
-wget $GEOJSON -o geo.json.new
+wget -i geo.json.url -O geo.json.new
 
 if cmp -s geo.json geo.json.net #returns 0 if identical
 then
-echo No change
+echo No change in GeoJSON
 exit 1
 fi
+echo GeoJSON is NEW
 rm geo.json
 mv geo.json.new geo.json
 exit 0
